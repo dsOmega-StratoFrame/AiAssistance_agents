3f4dd9fe1bdb973c079e28dd9d24ab7528b2526c
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-03-20 01:23:54 +0300

    [Enh](Zotero): Finished algorithm for generating paths structure.

--- src/kbn/zotero.py
+++ src/kbn/zotero.py
@@ -1,20 +1,34 @@
 from pyzotero import Zotero
 
+IS_DEBUG = True
+
+type TCollectionKey = str
+
+LIBRARY_ID = "0"
+
+def get_library_name(library_id):
+    if library_id == "0":
+        return "Library"
+
+    raise NotImplementedError('Fetch library name for non-default libraries')
+
+
 # Require Zotero 7+ with local API access enabled:
 # Zotero > Settings > Advanced > "Allow other applications on this computer to communicate with Zotero".
-zot = Zotero(library_id="0", library_type="user", local=True)
+zot = Zotero(library_id=LIBRARY_ID, library_type="user", local=True)
 
 items = zot.top(limit=5)
-
 
 # we've retrieved the latest five top-level items in our library
 # we can print each item's item type and ID
 for c in items:
     # print(item['data'])
-    print(f"Item: {c['data']['title']}")
+    print(f"Item: {c['data'].get('title')}")
 
 
+# [ {key, data, ...} ]
 collections = zot.collections(limit=2000)
+# { key: data }
 collections_map = {}
 
 for c in collections:
@@ -22,68 +36,99 @@ for c in collections:
     print(c['key'], c['data']['name'], c['data']['parentCollection'])
     # print(f"Item: {c['data']['title']}")
 
+# Collection utils.
+def get_name(key: TCollectionKey) -> str:
+    return collections_map.get(key, { 'name': get_library_name(LIBRARY_ID) })['name']
+
+def format_segments_as_path(path_segments: list[TCollectionKey]) -> str:
+    path = [get_name(segment) for segment in path_segments]
+
+    return '/'.join(path)
+
 print(len(collections))
 
+# { key: parentKey }
 tree = {}
-result = {}
-"""
-parent can be `False` if the parent is the library root
-{ parent: key }
-Iterate through each entry. For each check additional structure:
-{ lastLeaf: branch }
-
-For example:
-{
-B: A(.B)
-C: B(.C)
-}
-Check last of:
-    B = first of A.B => A.B.C.
-    C
 
 """
-for c in collections:
-    parentKey = c['data']['parentCollection']
-    if not parentKey:
-        print('no parent')
-        tree['Library'] = [c['key']]
-    else:
-        tree[parentKey] = [c['key']]
-    # del tree[False]
-    # print(f"Item: {c['data']['title']}")
+Parent can be `False` if the parent is the library root
+{ key: parent }
+Iterate through each entry. For each construct additional structure:
+{ key: branch }
 
-result = tree.pop('Library')
+For example, for Zotero structure:
+    Library/A/B1
+    Library/A/B2/C
+Do the following:
+    {
+        A: [Library],
+        B1: [A],
+        B2: [A],
+        C: [B2],
+    }
+    Check last leaf of each branch and try to connect with other existing branch:
+        - For A it will be [Library]. Last leaf is 'Library'. There's no entries by key 'Library'
+        - For B1 it will be [A]. Last leaf is 'A'. There's an entry `A: [Library]`. Merge them
+          and get:
+            {
+                A: [Library],
+                B1: [A, Library],
+                B2: [A],
+                C: [B2],
+            }
+        ...and so on until you get:
+            {
+                A: [Library],
+                B1: [A, Library],
+                B2: [A, Library],
+                C: [B2, A, Library],
+            }
 
-for [parent, branch] in tree.items():
-    print(f"{parent}: {branch}")
-    last_leaf = branch[-1]
+As you can se, result is reversed.
+"""
+def construct_paths_structure():
+    for c in collections:
+        parentKey = c['data']['parentCollection']
+        if not parentKey:
+            if IS_DEBUG:
+                print('no parent', c)
+            tree[c['key']] = [get_library_name(LIBRARY_ID)]
+        else:
+            tree[c['key']] = [parentKey]
 
-    branch = tree.get(last_leaf)
-    while branch:
-        tree[parent] += branch
-
+    for [parent, branch] in tree.items():
+        if IS_DEBUG:
+            print(f"{get_name(parent)} ({parent}): {format_segments_as_path(branch)} ({branch})")
         last_leaf = branch[-1]
-        branch = tree.get(last_leaf)
 
-# for [parent, branch] in tree.items():
-#     print(f"{parent}: {branch}")
+        child_branch = tree.get(last_leaf)
+        while child_branch:
+            tree[parent] += child_branch
 
-collections_paths = []
+            last_leaf = child_branch[-1]
+            child_branch = tree.get(last_leaf)
 
-for [parent, branch] in tree.items():
-    path_segments = [parent] + branch
+construct_paths_structure()
 
-    path = [collections_map[segment]['name'] for segment in path_segments]
 
-    path_str  = '/'.join(path)
-    collections_paths.append(path_str)
+def debug():
+    collections_paths = []
 
-    print(f"${path_str} | {parent}: {branch}")
+    for [parent, branch] in tree.items():
+        path_segments = [parent] + branch
+        path_segments.reverse()
 
-collections_paths.sort()
-print('------ sorted paths -----')
-for path in collections_paths:
-    print(path)
+        path = format_segments_as_path(path_segments)
+        collections_paths.append(path)
+
+        print(f"{path} | {parent}: {branch}")
+
+    collections_paths.sort()
+    print('------ sorted paths -----')
+    for path in collections_paths:
+        print(path)
+
+if IS_DEBUG:
+    debug()
 
 # print(tree)
-# print(result)
