d9252de0528486291e32da1679a476e8695d0936
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-04-11 19:02:00 +0300

    [Enh]: Defined abstract ChatNode with template, get_context, prompt.

--- src/nodes/base.py
+++ src/nodes/base.py
@@ -1,15 +1,60 @@
-template = """
-You are an expert in answering questions about a pizza restaurant
+from abc import ABC, abstractmethod
+from typing import Callable, override
 
-Here are some relevant reviews: {context}
+from langchain_core.embeddings import Embeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.vectorstores import VectorStoreRetriever
 
-Here is the question to answer: {question}
-"""
+from data_sources.base import DataSource
 
-def get_context(question: str) -> str:
-    return retriever.invoke(question)
 
-prompt = select_template(PIZZA_TEMPLATE)
+class ChatNode(ABC):
+    """Get context in any way possible."""
 
-class ChatNode:
-    
+    @property
+    @abstractmethod
+    def template(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_context_factory(self, question: str) -> str:
+        pass
+
+    def get_prompt(self) -> ChatPromptTemplate:
+        print("Following template:")
+        print(self.template)
+
+        return ChatPromptTemplate.from_template(self.template)
+
+
+class VectorRetrieverChatNode(ChatNode, metaclass=ABC):
+    """Get context using vector retriever."""
+
+    def __init__(
+        self,
+        data_source: DataSource,
+        retriever: VectorStoreRetriever,
+    ):
+        self.data_source = data_source
+        self.retriever = retriever
+
+    @property
+    @override
+    @abstractmethod
+    def template(self) -> str:
+        pass
+
+    @override
+    def get_context_factory(self, embeddings: Embeddings) -> Callable[[str], str]:
+        retriever = self.data_source.get_retriever(embeddings=embeddings)
+
+        def get_context(question: str) -> str:
+            return retriever.invoke(question)
+
+        return get_context
+
+    def get_prompt(self) -> ChatPromptTemplate:
+        print("Following template:")
+        print(self.template)
+
+        return ChatPromptTemplate.from_template(self.template)
